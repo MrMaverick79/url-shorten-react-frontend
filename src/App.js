5c9733532e9e0800db36ed432b156c991d5c0533
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-10-30 11:45:03 +1100

    Css styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ function App() {
       <header className="App-header">
         </header>
         <Short/>
-
+      <footer>
+        <a href="https://github.com/MrMaverick79/url-shorten-react-frontend" target="_blank" rel="noopener noreferrer">
+          <i className="devicon-github-original-wordmark colored"></i>
+        </a>
+        
+        <p>Brendan Tuckerman 2022</p>
+      </footer>
     </div>
   );
 }
--- src/Components/Short.jsx
+++ src/Components/Short.jsx
@@ -1,25 +1,55 @@
 import { useState } from "react";
 import axios from "axios";
+import "../App.css"
 const BASE_URL = process.env.REACT_APP_BASE_URL 
 
-
 const Short = () => {
-
+    //Main component to shorten a url
+    
+    //State
     const [originalUrl, setOriginalUrl] = useState("");
     const [shortUrl, setShortUrl] = useState();
 
-    function handleSubmit(e){
+    //Methods
+    function handleSubmit(){
         console.log(originalUrl);
         shortenUrl(originalUrl);
        
     }
 
     function handleChange(e){
-
         setOriginalUrl(e.target.value)
-
     }
 
+    function copyLink(){
+
+        const link = document.querySelector(".shortLink").innerHTML;
+        console.log("Link", link);
+
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(link);
+        
+        //Display hover text
+        let tooltip = document.getElementById("myTooltip");
+        tooltip.innerHTML = "Copied: " + link;
+
+    }// copyLink()
+
+    function outFunc() { //change hover text
+        let tooltip = document.getElementById("myTooltip");
+        tooltip.innerHTML = "Copy to clipboard";
+    }
+
+    
+    function clearAll(){
+        setShortUrl("");
+        setOriginalUrl("");
+        document.querySelector(".urlInput").value = ""
+    }
+
+ 
+
+    //Submit a post request to the back end
     async function shortenUrl(urlToShorten){
 
         try{
@@ -27,24 +57,47 @@ const Short = () => {
                 originalUrl: urlToShorten
             });
 
+            // console.log('A response has been received from the server', res.data);
+            //Set shortened url data to state
             setShortUrl(res.data)
-
-        } catch (err ){
-
+            
+        } catch ( err ){
             console.log('Unable to shorten this url', err);
-
         }
 
-
-    }
+    } //shortenUrl()
 
 
     return(
 
         <div className="wrapper" >
-            <h2>Shorten url here</h2>
-            <input type="text" onChange={handleChange}></input>
-            <button onClick={handleSubmit}>Generate</button>
+            <h1>shrt.URL</h1>
+            <div className="mainFocus">
+                <input type="text" onChange={handleChange} placeholder="Add a url you want to shorten here." className="urlInput"></input>
+                <button onClick={handleSubmit} onMouseOut={outFunc}>Shorten</button>
+                {
+                    shortUrl ?
+                    <button onClick={clearAll}>New</button>: null
+
+                }
+                {
+                shortUrl ? 
+                
+                <div className="results">
+                    <a href={shortUrl.shortUrl} className="shortLink">{shortUrl.shortUrl}</a>
+                    <div className="tooltip">
+                        <button onClick={copyLink} onMouseOut={outFunc}>
+                            <span className="tooltiptext" id="myTooltip">Copy to clipboard</span>
+                        Copy text
+                        </button> 
+                    </div> 
+                </div>
+                :
+                null
+
+                }
+
+            </div>
         </div>
     )
 
